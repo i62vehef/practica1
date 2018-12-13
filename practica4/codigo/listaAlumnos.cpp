a51fa14cb416d5c90d6602ecb912e3d6ee6b9ac2
i62vehef <pacovh8@gmail.com>
2018-12-13 12:08:34 +0100

    primer arreglo buscar

--- practica4/codigo/listaAlumnos.cpp
+++ practica4/codigo/listaAlumnos.cpp
@@ -4,7 +4,7 @@ std::vector<Alumno> ListaAlumnos::buscarAlumno(int const &criterio, string &valo
 {
 	//vector auxiliar para guardar los alumnos encontrados
 	//valdría con una variable Alumno si solo existe un alumno
-	std::vector<Alumno> aux;
+	std::vector<Alumno> aux(0);
 
 	//variable para elegir el alumno en caso de coincidencia multiple
 	int opcion=0;
@@ -23,22 +23,26 @@ std::vector<Alumno> ListaAlumnos::buscarAlumno(int const &criterio, string &valo
 			if(aux.size()>1)//Solo deberia haber un unico alumno con el mismo DNI
 			{
 				std::cout<<BIRED<<"ERROR se han encontrado mas de un alumno con el mismo DNI"<<RESET<<std::endl;
-				break;
+				return NULL;
 			}
 			else
 				return aux;
 		break;
 		case 2://Apellidos
 
+			//Se busca el alumno
 			for(int i=0;i<tamClase();i++)
 			{
 				if(strcmp(getAlumno(i).getDNI().c_str(),valor.c_str())==0)
 					aux.push_back(getAlumno(i));
 			}
+
+			//Comprobacion del numero de alumnos encontrados
 			if(aux.size()>1)
 			{
 				std::cout<<BIRED<<"Se han encontrado mas de un alumno con Apellidos coincidentes"<<RESET<<std::endl;
 
+				//Seleccion del alumno deseado
 				std::cout<<"Los alumnos son los siguientes:"<<std::endl;
 				std::cout<<"(Selecciona mediante el indice indicado el alumno que buscaba)"<<std::endl;
 
@@ -47,6 +51,7 @@ std::vector<Alumno> ListaAlumnos::buscarAlumno(int const &criterio, string &valo
 				
 				std::cin>>opcion;
 				
+				//mientras no se introduzca un alumno valido se sigue pidiendo uno
 				while(opcion<1 || opcion>aux.size())
 				{
 					std::cout<<BIRED<<"ERROR eleccion invalida"<<RESET<<std::endl;
@@ -58,6 +63,11 @@ std::vector<Alumno> ListaAlumnos::buscarAlumno(int const &criterio, string &valo
 					
 					std::cin>>opcion;
 				}
+
+				//se borran del vector de alumnos encontrados todos los no deseados
+				for(int i=0;i<aux.size();i++)
+					if(i!=opcion)
+						aux.erase(aux.begin()+i);
 
 			}
 		break;
