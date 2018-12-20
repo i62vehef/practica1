37b58de3e365d3a50e701df62e4add61f41fb42d
i62vehef <pacovh8@gmail.com>
2018-12-20 12:49:46 +0100

    añadido carpeta Build con ejecutable

---
+++ practica4/codigo/Build/CMakeFiles/Principal.exe.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/Fecha.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/Fecha.cpp.o"
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/alumno.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/alumno.cpp.o"
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/funcionesAuxiliares.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/funcionesAuxiliares.cpp.o"
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/listaAlumnos.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/listaAlumnos.cpp.o"
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/principal.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/principal.cpp.o"
+  "/home/poleifor13/Documentos/IS 3/practica4/codigo/profesor.cpp" "/home/poleifor13/Documentos/IS 3/practica4/codigo/Build/CMakeFiles/Principal.exe.dir/profesor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
